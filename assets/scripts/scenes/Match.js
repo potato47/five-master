ea8e267d83fcd56310c88a2015f86428c3298c63
potato47 <1406099478@qq.com>
2017-04-07 15:08:30 +0800

    build the main sample scenes

---
+++ assets/scripts/scenes/Match.js
@@ -0,0 +1,27 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,      // The default value will be used only when the component attaching
+        //                           to a node for the first time
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        setTimeout(()=>{
+            this.onMatchSuccess();
+        },2000);
+    },
+
+    onMatchSuccess(){
+        cc.director.loadScene("game");
+    }
+});
